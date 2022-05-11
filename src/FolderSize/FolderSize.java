bf135663fac2d26874b340ca65230c65bf13e043
DonThomaZ <zarzecznysa@gmail.com>
2022-05-11 16:23:42 +0200

    bowls

---
+++ src/FolderSize/FolderSize.java
@@ -0,0 +1,10 @@
+package FolderSize;
+
+import java.util.Scanner;
+
+public class FolderSize {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
